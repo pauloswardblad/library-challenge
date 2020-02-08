2b9f2eadd328dc9e381083d63d063dca41c103ae
pauloswardblad <paulo@swardblads.se>
2020-02-08 16:20:09 +0100

    added the it in the specfile

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -1,1 +1,48 @@
 require './lib/library.rb'
+describe Library do
+    it 'has a list of books' do
+        
+    end
+
+    it 'can search book by author' do
+        
+    end
+
+    it 'can search book by title' do
+        
+    end
+
+    it 'can check if the book is avaliable' do
+        
+    end
+
+    it 'can generate checkout date' do
+        
+    end
+
+    it 'can generate a return date' do
+        
+    end
+
+    it 'when book checked out availability changes to unavailable' do        
+    
+    end
+
+    it 'when book return the avaliablity changes to avaliable'
+
+    it 'when checkout occurs, visitor pinged return date information' do        
+    
+    end
+
+
+
+
+
+
+
+
+
+
+
+
+end
