c007039ee095c176af12b40cd352cfdc8800dc93
pauloswardblad <paulo@swardblads.se>
2020-02-09 01:03:39 +0100

    half past the available test(3), tested in irb, when i change to false in step 3 and write it to file, it add a new line of available in the item in data.yml

--- lib/library.rb
+++ lib/library.rb
@@ -1,53 +1,44 @@
 require 'yaml'
 
 class Library
-<<<<<<< HEAD
-<<<<<<< HEAD
-    require 'yaml'  
-
-    attr_accessor :index
-
-    #STANDARD_RETURN_DATE = 30
-
-    def initialize     
-        @index = YAML.load_file('./lib/data.yml')
-        #@index = nil 
-    end
-
-end
-=======
-  require 'yaml'
-=======
->>>>>>> afbcd7eebfba172306fb8de8ecc349882dcf63ed
+#<<<<<<< HEAD
 
   attr_accessor :index
+
+  
 
   #STANDARD_RETURN_DATE = 30
 
   def initialize
-    @index = YAML.load_file('./lib/data.yml')
+        @index = YAML.load_file('./lib/data.yml')
   end
 
   def query_title(title)
-      index.select { [:item][:title].include? "#{title}" }
+        @index.select { [:item][:title].include? "#{title}" }
   end
   
   def query_author(author)
-    index.select { [:item][:author].include? "#{author}" }
+        @index.select { [:item][:author].include? "#{author}" }
   end
 
   def is_available(available)
-    index.select { [:item][:available].eq? "true" }
+        @index.select { |book| book[:available] == true }
+        #index.select { [:item][:available].to eq? "true" } #to be_truthy? "#{available}" } 
   end
 
- # def return_date
-  #  Date.today.next_year(Account::STANDARD_VAILIDITY_YRS).strftime('%m/%y')
+  #def checkout_date()
+        #empty
   #end
+
+  #def return_date(return_date)
+        #Date.today.next_month (Library::STANDARD_VAILIDITY_DATE).strftime('%m/%y')
+  #end
+
+
 
 end
 
 # to see index in irb p @index = YAML.load_file('./lib/data.yml')
-#correcting, pushing up so i can get the new file from blake
 
 
->>>>>>> d5e183041dd7239a44acef56e71b1866342962f3
+#>>>>>>> d5e183041dd7239a44acef56e71b1866342962f3
--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -4,32 +4,35 @@ require 'yaml'
 describe Library do
 
   it 'has a list of books' do
-    expect(subject.index).not_to eq nil
+        expect(subject.index).not_to eq nil
   end
 
   it 'can search book list by title' do
-    expect(subject.index[1][:item][:title]).to include("Skratta la")
+        expect(subject.index[1][:item][:title]).to include("Skratta la")
   end
 
 
   it 'can search book list by author' do
-    expect(subject.index[2][:item][:author]).to include("Gunilla")
+        expect(subject.index[2][:item][:author]).to include("Gunilla")
   end
 
 
   it 'can check if the book is available' do
-    expect(subject.index[2][:item][:available]).to eq("true")
+        expected_output = YAML.load_file('./lib/data.yml').select { |book| book[:available] == true }
+        expect(subject.index).to eq expected_output
+        #expect(subject.index[2][:item][:available]).to eq ("true") #be_truthy
   end
   
-=begin 
-  it 'can generate checkout date' do
+  
+  #it 'can generate checkout date' do
     
-  end
+  #end
 
-  it 'can generate return date' do
-    
-  end
-
+  #it 'can generate return date' do
+        #expected_output = YAML.load_file('./lib/data.yml').select { |returnbook| item[:return_date] == return_date }
+        #expect(subject.index).to eq expected_output
+  #end
+=begin
   it 'when book checked out availability changes to unavailable' do
     
   end
